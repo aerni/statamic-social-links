5a15487c4ec9c861edda701cdc4b02c738d88929
Michael Aerni <hello@michaelaerni.ch>
2020-07-07 15:01:03 +0200

    Add basic functionality

---
+++ Social/SocialTags.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Statamic\Addons\Social;
+
+use Statamic\Extend\Tags;
+use Illuminate\Support\Facades\Request;
+
+class SocialTags extends Tags
+{
+    /**
+     * Maps to {{ social:channel }}
+     *
+     * Where `channel` is the name of the social channel
+     *
+     * @param $method
+     * @param $args
+     * @return string
+     */
+    public function __call($method, $args)
+    {
+        $channel = explode(':', $this->tag, 2)[1];
+
+        return $this->createLink($channel);
+    }
+
+    /**
+     * Creates the social sharing links
+     *
+     * @param string $channel
+     * @return string
+     */
+    public function createLink(string $channel): string
+    {
+        $params = $this->getParams();
+
+        if ($channel === 'facebook') {
+            return "https://www.facebook.com/sharer/sharer.php?u={$params['url']}";
+        }
+
+        if ($channel === 'twitter') {
+            return "https://twitter.com/intent/tweet?url={$params['url']}&text={$params['title']}&via={$params['handle']}";
+        }
+
+        if ($channel === 'linkedin') {
+            return "https://www.linkedin.com/shareArticle?mini=true&url={$params['url']}&title={$params['title']}&summary={$params['text']}&source={$params['source']}";
+        }
+
+        if ($channel === 'whatsapp') {
+            return "whatsapp://send?text={$params['url']}";
+        }
+
+        if ($channel === 'mail') {
+            return "mailto:{$params['mailto']}?&cc={$params['cc']}&bcc={$params['bcc']}&subject={$params['subject']}&body={$params['text']}";
+        }
+    }
+
+    /**
+     * Get all the parameters from the tag
+     *
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return [
+            'url' => $this->getParam('url') ?? Request::fullUrl(),
+            'title' => rawurlencode($this->getParam('title')),
+            'text' => rawurlencode($this->getParam('text')),
+            'source' => rawurlencode($this->getParam('source')),
+            'handle' => rawurlencode($this->getParam('handle')),
+            'mailto' => rawurlencode($this->getParam('mailto')),
+            'cc' => rawurlencode($this->getParam('cc')),
+            'bcc' => rawurlencode($this->getParam('bcc')),
+            'subject' => rawurlencode($this->getParam('subject')),
+        ];
+    }
+
+}
